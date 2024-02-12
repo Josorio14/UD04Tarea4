5cbe51013a61c95a498850a9f2dd2f4fd341c399
Josorio14 <148210970+Josorio14@users.noreply.github.com>
2024-02-12 11:46:57 +0100

    Mer4

---
+++ Insert4.sql
@@ -0,0 +1,3 @@
+use MER4;
+INSERT INTO Polissa (Num_Polissa, Precio_Anual, Tipos_Pagamiento, Fecha_contratacion, Fecha_Inicio_Vigencia, Fecha_Final_Vigencia, Cliente_DNI, Tipos_Polissa_ID, Vendedor_ID)
+VALUES ('20', '449,99€','anual', '2021-05-05','2022-05-09','2023-05-09','35324512P','5','20');
