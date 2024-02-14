c7be7f690a26eabf3d974bf26dfc109c885190c1
Josorio14 <148210970+Josorio14@users.noreply.github.com>
2024-02-14 09:36:43 +0100

    Ejercicios

---
+++ InsertMER5.sql
@@ -0,0 +1,4 @@
+use MER5;
+INSERT INTO Cites (ID, Matricula_Vehicle, Data, Hora, Vehicle_ID, Vehicle_Tipus_Vehicle_ID, Client_DNI)
+VALUES ('15','4563 CBI','2021-03-19','19:15','10','2','69504226C')
+
